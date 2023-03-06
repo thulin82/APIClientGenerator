c17a05ebef25f4a299ad73f53b65417137e188f1
Markus Thulin <macky_b@hotmail.com>
2023-03-06 16:37:53 +0100

    Started with 2 arguments

--- APIClientGenerator/Program.cs
+++ APIClientGenerator/Program.cs
@@ -5,6 +5,23 @@
         static async Task Main(string[] args)
         {
 
+            if (args.Length != 2)
+                throw new ArgumentException("Expecting 2 arguments: filepath/URL, language");
+
+            var url = args[0];
+            var language = args[1];
+            var generatePath = Directory.GetCurrentDirectory();
+
+            if (language != "TypeScript" && language != "CSharp")
+                throw new ArgumentException("Invalid language parameter; valid values are TypeScript and CSharp");
+
+            if (language == "TypeScript"){
+                generatePath = Path.Combine(generatePath, "client.ts");
+                await Generator.GenerateTypeScriptClientUrl(url, generatePath);
+            }
+            else
+                generatePath = Path.Combine(generatePath, "Client.cs");
+                await Generator.GenerateCSharpClientUrl(url, generatePath);
         }
     }
 }
