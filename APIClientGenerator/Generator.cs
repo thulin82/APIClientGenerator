c609bae4229d710add38f1089d2ce2b9d013deb1
Markus Thulin <macky_b@hotmail.com>
2023-03-09 08:37:02 +0100

    Added private helping methods to Generator

--- APIClientGenerator/Generator.cs
+++ APIClientGenerator/Generator.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NJsonSchema.CodeGeneration.TypeScript;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+using NSwag.CodeGeneration.TypeScript;
 
 namespace APIClientGenerator
 {
-    internal class Generator
+    public static class Generator
     {
+
+
+        private async static Task GenerateClient(OpenApiDocument document, string generatePath, Func<OpenApiDocument, string> generateCode)
+        {
+            Console.WriteLine($"Generating {generatePath}...");
+
+            var code = generateCode(document);
+
+            await System.IO.File.WriteAllTextAsync(generatePath, code);
+        }
+
+        private static CSharpClientGeneratorSettings CSharpGeneratorSettings()
+        {
+            return new CSharpClientGeneratorSettings
+            {
+                UseBaseUrl = false
+            };
+        }
+
+        private static TypeScriptClientGeneratorSettings TypeScriptGeneratorSettings()
+        {
+            var settings = new TypeScriptClientGeneratorSettings();
+            settings.TypeScriptGeneratorSettings.TypeStyle = TypeScriptTypeStyle.Interface;
+            settings.TypeScriptGeneratorSettings.TypeScriptVersion = 3.5M;
+            settings.TypeScriptGeneratorSettings.DateTimeType = TypeScriptDateTimeType.String;
+            return settings;
+        }
     }
 }
