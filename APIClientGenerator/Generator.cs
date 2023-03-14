8d59f05175ba6ef267e65ffb201be5bb15bf861f
Markus Thulin <macky_b@hotmail.com>
2023-03-14 14:21:20 +0100

    Added settings and generators for both typescript and c#

--- APIClientGenerator/Generator.cs
+++ APIClientGenerator/Generator.cs
@@ -7,8 +7,87 @@ namespace APIClientGenerator
 {
     public static class Generator
     {
+        /// <summary>
+        /// Generates a TypeScript client from a URL
+        /// </summary>
+        /// <param name="url">The URL of the OpenAPI document</param>
+        /// <param name="generatePath">The path to generate the client to</param>
+        /// <returns></returns>
+        async public static Task GenerateTypeScriptClientUrl(string url, string generatePath) =>
+            await GenerateClient(
+                document: await OpenApiDocument.FromUrlAsync(url),
+                generatePath: generatePath,
+                generateCode: (OpenApiDocument document) =>
+                {
+                    var generator = new TypeScriptClientGenerator(document, TypeScriptGeneratorSettings());
+                    var code = generator.GenerateFile();
 
+                    return code;
+                }
+            );
 
+        /// <summary>
+        /// Generates a TypeScript client from a file
+        /// </summary>
+        /// <param name="path">The path of the OpenAPI document</param>
+        /// <param name="generatePath">The path to generate the client to</param>
+        /// <returns></returns>
+        async public static Task GenerateTypeScriptClientFile(string path, string generatePath) =>
+            await GenerateClient(
+                document: await OpenApiDocument.FromFileAsync(path),
+                generatePath: generatePath,
+                generateCode: (OpenApiDocument document) =>
+                {
+                    var generator = new TypeScriptClientGenerator(document, TypeScriptGeneratorSettings());
+                    var code = generator.GenerateFile();
+
+                    return code;
+                }
+            );
+
+        /// <summary>
+        /// Generates a C# client from a URL
+        /// </summary>
+        /// <param name="url">The URL of the OpenAPI document</param>
+        /// <param name="generatePath">The path to generate the client to</param>
+        /// <returns></returns>
+        async public static Task GenerateCSharpClientUrl(string url, string generatePath) =>
+            await GenerateClient(
+                document: await OpenApiDocument.FromUrlAsync(url),
+                generatePath: generatePath,
+                generateCode: (OpenApiDocument document) =>
+                {
+                    var generator = new CSharpClientGenerator(document, CSharpGeneratorSettings());
+                    var code = generator.GenerateFile();
+                    return code;
+                }
+            );
+
+        /// <summary>
+        /// Generates a C# client from a file
+        /// </summary>
+        /// <param name="path">The path of the OpenAPI document</param>
+        /// <param name="generatePath">The path to generate the client to</param>
+        /// <returns></returns>
+        async public static Task GenerateCSharpClientFile(string path, string generatePath) =>
+            await GenerateClient(
+                document: await OpenApiDocument.FromFileAsync(path),
+                generatePath: generatePath,
+                generateCode: (OpenApiDocument document) =>
+                {
+                    var generator = new CSharpClientGenerator(document, CSharpGeneratorSettings());
+                    var code = generator.GenerateFile();
+                    return code;
+                }
+            );
+
+        /// <summary>
+        /// Generates a client from an OpenAPI document and writes it to a file
+        /// </summary>
+        /// <param name="document">The OpenAPI document</param>
+        /// <param name="generatePath">The path to generate the client to</param>
+        /// <param name="generateCode">The function to generate the client code</param>
+        /// <returns></returns>
         private async static Task GenerateClient(OpenApiDocument document, string generatePath, Func<OpenApiDocument, string> generateCode)
         {
             Console.WriteLine($"Generating {generatePath}...");
@@ -18,14 +97,23 @@ namespace APIClientGenerator
             await System.IO.File.WriteAllTextAsync(generatePath, code);
         }
 
+        /// <summary>
+        /// Returns the C# client generator settings
+        /// </summary>
+        /// <returns></returns>
         private static CSharpClientGeneratorSettings CSharpGeneratorSettings()
         {
             return new CSharpClientGeneratorSettings
             {
-                UseBaseUrl = false
+                UseBaseUrl = false,
+                GenerateSyncMethods = true
             };
         }
 
+        /// <summary>
+        /// Returns the TypeScript client generator settings
+        /// </summary>
+        /// <returns></returns>
         private static TypeScriptClientGeneratorSettings TypeScriptGeneratorSettings()
         {
             var settings = new TypeScriptClientGeneratorSettings();
